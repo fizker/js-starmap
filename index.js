0e061fb53905a490d3f0d17acc195bfa686f1746
Benjamin Horsleben <benjamin@fizkerinc.dk>
2013-06-30 17:05:35 +0200

    Basic setup.

---
+++ index.js
@@ -0,0 +1,14 @@
+var PORT = process.env.PORT || 8080
+var fs = require('fs')
+var express = require('express')
+var app = express()
+
+app.use(express.static('./client'))
+app.get('/', function(req, res) {
+	fs.readFile('./client/index.html', 'utf8', function(err, html) {
+		res.send(html)
+	})
+})
+app.listen(PORT, function() {
+	console.log('App running at ' + PORT)
+})
