938f90a8885a24aee66eefc7e562cd89b5ede6e9
Benjamin Horsleben <benjamin@fizkerinc.dk>
2013-07-01 08:48:54 +0200

    Extended the tests for the dom manipulator. Added a $.create method for easily creating dom nodes.

--- client/dom.js
+++ client/dom.js
@@ -1,7 +1,9 @@
 var $ = (function() {
 	var slice = Array.prototype.slice
 	var concat = Array.prototype.concat
-	return function $(elements, scope) {
+	var creator = document.createElement('div')
+
+	function $(elements, scope) {
 		if(typeof(elements) == 'string') {
 			elements = (scope || document).querySelectorAll(elements)
 			elements = slice.call(elements)
@@ -9,25 +11,49 @@ var $ = (function() {
 		if(!elements) {
 			elements = []
 		}
+		if(elements instanceof DocumentFragment) {
+			elements = slice.call(elements.childNodes)
+		}
 		if(!Array.isArray(elements)) {
 			elements = [ elements ]
 		}
 		return extend(elements)
 	}
+	$.create = createNode;
+	return $;
+
+	function createNode(str) {
+		creator.innerHTML = str
+		var elms = document.createDocumentFragment()
+		while(creator.firstChild) {
+			elms.appendChild(creator.firstChild)
+		}
+		return $(elms)
+	}
 
 	function extend(arr) {
-		arr.on = function(event, fn) {
+		extend = function extend(arr) {
+			if(arr._extended) {
+				return arr
+			}
+			arr._extended = true
+			arr.on = on
+			arr.find = find
+			return arr
+		}
+		return extend(arr)
+
+		function find(sel) {
+			return concat.apply([], this.map(function(el) {
+				return $(sel, el)
+			}))
+		}
+		function on(event, fn) {
 			this.forEach(function(el) {
 				el.addEventListener(event, fn)
 			})
 			return this
 		}
-		arr.find = function(sel) {
-			return concat.apply([], this.map(function(el) {
-				return $(sel, el)
-			}))
-		}
-		return arr
 	}
 })()
 function $$(sel, scope) {
--- test/integration/dom.js
+++ test/integration/dom.js
@@ -15,6 +15,43 @@ describe('integration/dom.js', function() {
 			it('calls `querySelectorAll` on `document`', function() {
 				expect(document.querySelectorAll).to.have.been.called
 			})
+			it('returns an extended array', function() {
+				expect(result).to.be.an('array')
+					.and.have.property('_extended', true)
+			})
+		})
+	})
+
+	describe('When calling `$.create()`', function() {
+		var elments
+		describe('with a single element', function() {
+			beforeEach(function() {
+				elements = $.create('<div></div>')
+			})
+			it('should return a list', function() {
+				expect(elements).to.be.an('array')
+					.and.have.length(1)
+			})
+			it('should contain the element', function() {
+				expect(elements[0]).to.have.property('nodeName', 'DIV')
+			})
+			it('should be extended', function() {
+				elements.should.have.property('_extended', true)
+			})
+		})
+		describe('with multiple elements', function() {
+			beforeEach(function() {
+				elements = $.create('<div id="first"></div><div id="second"></div>')
+			})
+			it('should return an extended list', function() {
+				expect(elements).to.be.an('array')
+					.and.have.length(2)
+					.and.have.property('_extended', true)
+			})
+			it('should contain the correct divs', function() {
+				expect(elements[0]).to.have.property('id', 'first')
+				expect(elements[1]).to.have.property('id', 'second')
+			})
 		})
 	})
 })
